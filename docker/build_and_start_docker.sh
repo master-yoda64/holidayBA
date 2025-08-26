79b7f0bd5a5679100cd846ba55a085861d084af6
master-yoda <hyoda365@gmail.com>
2025-08-27 04:13:00 +0900

    modify docker

--- docker/build_and_start_docker.sh
+++ docker/build_and_start_docker.sh
@@ -3,5 +3,6 @@
 export UID="$(id -u)"
 export GID="$(id -g)"
 
-docker compose build --progress=plain --no-cache
+#docker compose build --progress=plain --no-cache
+docker compose build --progress=plain
 docker compose up -d --remove-orphans
